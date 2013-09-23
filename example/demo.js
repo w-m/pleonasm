b7adaf1fbf03256a173587d4ba58acb9e9bd5d1e
Wieland Morgenstern <git@morgenst.de>
2013-09-24 00:10:31 +0200

    npmignore, restored browser tests

--- example/demo.js
+++ example/demo.js
@@ -3,10 +3,12 @@ var pleonasm = require('pleonasm');
 
 pleonasm.onload(function() {
   
-  var encoded = pleonasm.encode('ad3a507');
-  console.log(encoded.code);
+  var sample_hex = 'ad3a507';
+  var encoded = pleonasm.encode(sample_hex);
+  console.log(sample_hex + ' encoded as: ' + encoded.code);
 
-  var decoded = pleonasm.decode('perform step gods, fear');
-  console.log(decoded.hex);
+  var sample_phrase = 'perform step gods, fear';
+  var decoded = pleonasm.decode(sample_phrase);
+  console.log(sample_phrase + ' decoded to: ' + decoded.hex);
 
 });
