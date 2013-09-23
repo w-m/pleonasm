f040e020ffdce0fd79045a59fd52343c639c0ff8
Wieland Morgenstern <git@morgenst.de>
2013-09-23 19:50:56 +0200

    jshint

--- js/pleonasm.js
+++ js/pleonasm.js
@@ -1,5 +1,7 @@
 var pleonasm = (function () {
   
+  'use strict';
+
   var module = {};
 
   var dicts = {};
@@ -10,7 +12,7 @@ var pleonasm = (function () {
   var hex_re  = new RegExp('[^' + hex_alphabet  + ']', 'g');
   var trsl_re = new RegExp('[^' + trsl_alphabet + ']', 'g');
 
-  var onload_callbacks = new Array();
+  var onload_callbacks = [];
 
   function checkReady() {
     if (checkDictionariesAvailable()) {
@@ -27,18 +29,18 @@ var pleonasm = (function () {
     } else {
       onload_callbacks.push(callback);
     }
-  }
+  };
 
   function initTree(id) {
     return function(lines) {
       buildTree(id, lines);
       checkReady();
-    }
+    };
   }
 
-  loadDict("adj.txt",  initTree('adj'));
-  loadDict("noun.txt", initTree('noun'));
-  loadDict("verb.txt", initTree('verb'));
+  loadDict('adj.txt',  initTree('adj'));
+  loadDict('noun.txt', initTree('noun'));
+  loadDict('verb.txt', initTree('verb'));
 
   var treeIDs = ['verb', 'adj', 'noun'];
 
@@ -67,11 +69,11 @@ var pleonasm = (function () {
 
   module.encode = function(hex, wordDelimiter, groupDelimiter) {
 
-    if(typeof(wordDelimiter)==='undefined') wordDelimiter = ' ';
-    if(typeof(groupDelimiter)==='undefined') groupDelimiter = ', ';
+    if(typeof(wordDelimiter) === 'undefined') { wordDelimiter = ' '; }
+    if(typeof(groupDelimiter) === 'undefined') { groupDelimiter = ', '; }
 
     if (!checkDictionariesAvailable()) {
-      return {code: "Loading Dictionaries..."};
+      return {code: 'Loading Dictionaries...'};
     }
 
     var result = {};
@@ -82,15 +84,15 @@ var pleonasm = (function () {
 
     result.translation = translated;
 
-    var treeIndex = 0; 
-    var codeWords = Array();
+    var treeIndex = 0;
+    var codeWords = [];
 
     while (translated.length > 0) {
 
       // verb, adj, noun, verb, adj, noun, ...
       var tree = dicts[treeIDs[treeIndex % treeIDs.length]];
 
-      longest = find_longest(tree, translated);
+      var longest = find_longest(tree, translated);
       translated = translated.substring(longest.depth);
 
       codeWords.push(longest.match);
@@ -103,19 +105,18 @@ var pleonasm = (function () {
     result.spaced = result.code.replace(trsl_re, ' ');
 
     return result;
-  }
+  };
 
   module.format = function(codeWords, wordDelimiter, groupDelimiter) {
     
-    var groups = Array();
+    var groups = [];
     var groupSize = treeIDs.length;
     for (var i = 0; i < Math.ceil(codeWords.length / groupSize); i++) {
-      subGroup = codeWords.slice(i * groupSize, (i * groupSize) + groupSize);
+      var subGroup = codeWords.slice(i * groupSize, (i * groupSize) + groupSize);
       groups.push(subGroup.join(wordDelimiter));
     }
     return groups.join(groupDelimiter);
-  }
-
+  };
 
   module.decode = function(code) {
 
@@ -123,8 +124,7 @@ var pleonasm = (function () {
     var spaced = code.replace(trsl_re, ' ');
     var hex = translate_to_hex(removed_redundancy);
     return {hex: hex, spaced: spaced, translation: removed_redundancy};
-  }
-
+  };
 
   function find_longest(tree, word) {
 
@@ -132,15 +132,15 @@ var pleonasm = (function () {
     var longest_match = null;
 
     for (var i = 0; i <= word.length; i++) {
-      sub_path = word.substring(0, i);
+      var sub_path = word.substring(0, i);
       if (sub_path in tree) {
         var match = tree[sub_path];
-        if (match != null) {
+        if (match !== null) {
           depth = i;
           longest_match = match;
         }
       } else {
-        break
+        break;
       }
     }
     return {depth: depth, match: longest_match};
@@ -150,12 +150,12 @@ var pleonasm = (function () {
     var client = new XMLHttpRequest();
     client.open('GET', 'dictionaries/' + dictFileName);
     client.onreadystatechange = function() {
-      if (client.readyState == 4) {
+      if (client.readyState === 4) {
         var plainText = client.responseText;
         var lines = plainText.split('\n');
         dataCallBack(lines);
       }
-    }
+    };
     client.send();
   }
 
@@ -173,17 +173,15 @@ var pleonasm = (function () {
       // e.g. b = null, bc = null when inserting bcd = braced
       // --> to know when to stop the lookup
       for (var j = 0; j < filtered_line.length; j++) {
-        sub_path = filtered_line.substring(0, j);
+        var sub_path = filtered_line.substring(0, j);
         if (!(sub_path in tree)) {
           tree[sub_path] = null;
         }
-      } 
-
+      }
       // actual lookup table
       tree[filtered_line] = lines[i];
     }
     dicts[dictID] = tree;
-  } 
-
+  }
   return module;
 }());
--- js/test.js
+++ js/test.js
@@ -3,39 +3,39 @@ function loadTest(testFileName, dataCallBack) {
   var client = new XMLHttpRequest();
   client.open('GET', 'tests/' + testFileName);
   client.onreadystatechange = function() {
-    if (client.readyState == 4) {
+    if (client.readyState === 4) {
       var plainText = client.responseText;
       var lines = plainText.split('\n');
-      document.body.innerHTML += ("<p>Loaded " + testFileName + "</p");
+      document.body.innerHTML += ('<p>Loaded ' + testFileName + '</p>');
       dataCallBack(lines);
     }
-  }
+  };
   client.send();
 }
 
 function check(source, code) {
-  encoded = pleonasm.encode(source, " ", " ").code;
-  decoded = pleonasm.decode(encoded).hex;
-  return ((encoded === code) && (decoded === source))
+  var encoded = pleonasm.encode(source, ' ', ' ').code;
+  var decoded = pleonasm.decode(encoded).hex;
+  return ((encoded === code) && (decoded === source));
 }
 
 function check_file(data) {
   
   var tests = 0;
   for (var i = 0; i < data.length; i++) {
-    line = data[i];
+    var line = data[i];
     if (!line) {
       continue;
     }
-    split = line.split('\t');
+    var split = line.split('\t');
     var success = check(split[0], split[1]);
     if (!success) {
-      alert("Test failed at " + split[0] + " / " + split[1]);
+      alert('Test failed at ' + split[0] + ' / ' + split[1]);
       return;
     }
     tests += 1;
   }
-  document.body.innerHTML += ("<p>" + tests + " test cases successful!" + "</p");
+  document.body.innerHTML += ('<p>' + tests + ' test cases successful!' + '</p>');
 }
 
 pleonasm.onload(function() {
